e134ccd5c749760414583201de57f8d10b6898f4
henriw22 <henriw22@gmail.com>
2021-07-06 15:57:12 -0700

    added and completed interviewerList.js with the styling and stories

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+// import classnames from 'classnames/bind';
+import InterviewerListItem from "components/InterviewerListItem";
+
+import "components/InterviewerList.scss";
+
+
+
+export default function InterviewerList(props) {
+  const [selectedInterviewer, setInterviewer] = useState(props.interviewer);
+  const interviewers = props.interviewers;
+
+  const interviewerList = interviewers.map((interviewer) => {
+    return <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={selectedInterviewer === interviewer.id}
+      // setInterviewer={event => setInterviewer(interviewer.id)}
+      setInterviewer={setInterviewer}
+    />
+  })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerList}</ul>
+    </section>
+  )
+
+}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,7 +10,7 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li key={props.id} className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
+    <li className={interviewerClass} onClick={() => props.setInterviewer()}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
