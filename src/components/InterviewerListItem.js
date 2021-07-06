28605cfb12118f44a367033e1b685a91337cd53b
henriw22 <henriw22@gmail.com>
2021-07-06 13:46:21 -0700

    completed interviewerListItem with styling and stories

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classnames from 'classnames/bind';
+
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li key={props.id} className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  )
+
+}
