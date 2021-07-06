433181788e1f02bd3ee0a486c7448702a4ddab1a
henriw22 <henriw22@gmail.com>
2021-07-05 18:34:28 -0700

    refactored Button.js, added DayListItem component and story

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,21 @@
 import React from "react";
+import classnames from 'classnames/bind';
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
- 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
- 
-   if (props.danger) {
-     buttonClass += " button--danger";
-   }
- 
-   return (
-      <button 
+  const buttonClass = classnames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return (
+    <button
       className={buttonClass}
       onClick={props.onClick}
       disabled={props.disabled}
-      >
+    >
       {props.children}
-      </button>
-   );
- }
+    </button>
+  );
+}
