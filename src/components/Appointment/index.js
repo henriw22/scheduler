9dc5b7c06b9e2e0232e1302f164825541fcc18e8
henriw22 <henriw22@gmail.com>
2021-07-06 17:50:12 -0700

    created Appointment, empty and header component, added styling and stories

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+import "./styles.scss";
+// import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+
+
+export default function Appointment(props) {
+
+  return <article className="appointment"></article>
+
+}
