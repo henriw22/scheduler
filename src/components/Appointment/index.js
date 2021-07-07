3ac7803465546085b956e8f40e92612179ba5194
henriw22 <henriw22@gmail.com>
2021-07-06 22:58:33 -0700

    completed form component and stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,10 +3,19 @@ import React, { useState } from "react";
 import "./styles.scss";
 // import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 
 export default function Appointment(props) {
+  const { student, interviewer } = props.interview || {};
 
-  return <article className="appointment"></article>
-
+  return (
+    <div className="appointment">
+      <Header time={props.time} />
+      {props.interview ? 
+        <Show student={student} interviewer={interviewer.name} /> : 
+        <Empty />}
+    </div>
+  )
 }
